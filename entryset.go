f1b5db0e49b0472c7fb23b4481774877a15de4cc
Exokem <ex010@live.com>
2025-09-03 16:57:43 -0400

    Export struct properties

--- entryset.go
+++ entryset.go
@@ -3,12 +3,12 @@ package main
 import "sync"
 
 type Entry struct {
-	name string
-	path string
+	Name string
+	Path string
 }
 
 type EntrySet struct {
-	index map[string]Entry
+	Index map[string]Entry
 	mu    sync.Mutex
 }
 
@@ -21,12 +21,20 @@ func NewEntrySet() *EntrySet {
 	return set
 }
 
-func (set *EntrySet) Store(name string, path string) *Entry {
+func (set *EntrySet) Store(name string, path string) Entry {
 	set.mu.Lock()
 	defer set.mu.Unlock()
 
-	entry := &Entry{name, path}
-	set.index[name] = *entry
-
+	entry := Entry{name, path}
+	set.Index[name] = entry
 	return entry
+}
+
+func (set *EntrySet) Absorb(other *EntrySet) *EntrySet {
+
+	for key, value := range other.Index {
+		set.Index[key] = value
+	}
+
+	return set
 }
